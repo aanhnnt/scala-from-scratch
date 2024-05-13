16fee5f2ed5a139121dce4c51c7ce285f01281d1
aanhnnt <86863241+aanhnnt@users.noreply.github.com>
2024-05-13 17:16:03 +0000

    Update setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-eval "$(cs install --env)"
+# Install coursier
+curl -fL https://github.com/coursier/coursier/releases/latest/download/cs-x86_64-pc-linux.gz | gzip -d > cs && chmod +x cs && ./cs setup -y
+echo "export PATH=$PATH:~/.local/share/coursier/bin" >> ~/.bashrc
+source ~/.bashrc
+rm -rf cs
+cs install scala:2.13.14 scalac:2.13.14
